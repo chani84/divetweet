25494596eb5d4a6e1a6884931868322fe581906c
RyuSeungChanne <ryuseungchanne@ryuushousannoMacBook-puro.local>
2019-01-15 15:32:36 +0900

    Log in-out devise

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,14 @@
 class ToppagesController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
     @tweets = Tweet.all
+  end
+
+  private
+  def move_to_index
+    redirect_to index: :action unless user_signed_in?
   end
 
 end
