0b116c8b82992d49e48e7caf5fcee816adbe7699
RyuSeungChanne <ryuseungchanne@ryuushousannoMacBook-puro.local>
2019-01-28 21:29:58 +0900

    revise

--- db/migrate/20190112111636_create_comments.rb
+++ db/migrate/20190112111636_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
       t.text               :content, null: false
-      t.references         :user, foreign_key:true
-      t.references         :tweet, foreign_key:true
+      # t.references         :user, foreign_key: true
+      # t.references         :tweet, foreign_key: true
       t.timestamps
     end
   end
